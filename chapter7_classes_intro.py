6de5c99fc1abe40fbdf9c4898ad2a6af52d6de60
Yisel M Noa <yisel05371@gmail.com>
2024-01-15 22:49:59 -0500

    more examples of classes

--- chapter7_classes_intro.py
+++ chapter7_classes_intro.py
@@ -46,4 +46,76 @@ r = np.linspace(0, 1, 9)
 v = v1.value(r)
 print(v)
 
-# Example 3: how can you keep bank accounts in a an organized manner
+
+# Example 3: how can you keep bank accounts in a an organized manner. The underscore _ before a name indicates protection, so no attribute can be touched and not method should be called
+
+class Account:
+    def __init__(self, name, account_number, initial_amount):
+        self._name = name
+        self._no = account_number
+        self._balance = initial_amount
+
+    def deposit(self, amount):
+        self._balance += amount
+
+    def withdraw(self, amount):
+        self._balance -= amount
+
+    def get_balance(self):
+        """to only read the balance attribute"""
+        return self._balance
+
+    def dump(self):
+        s = '%s, %s, balance:%s' % (self._name, self._no, self._balance)
+        print(s)
+
+
+a1 = Account('John Smith', '123456789', 500000)
+print(a1)
+a1.deposit(100000)
+a1.withdraw(50000)
+print(a1._balance)
+
+
+# Example 4, set up initially attributes = None to add them later with the corresponding methods
+class Phone_book:
+    def __init__(self, name, mobile_phone=None, office_phone=None, private_phone=None, email=None):
+        self.name = name
+        self.mobile = mobile_phone
+        self.office = office_phone
+        self.private = private_phone
+        self.email = email
+
+    def add_mobile_phone(self, number):
+        self.mobile = number
+
+    def add_office_phone(self, number):
+        self.office = number
+
+    def add_private_phone(self, number):
+        self.private = number
+
+    def add_email(self, address):
+        self.email = address
+
+    def dump(self):
+        s = self.name + '\n'
+        if self.mobile is not None:
+            s += self.mobile + '\n'
+        if self.office is not None:
+            s += self.office + '\n'
+        if self.private is not None:
+            s += self.private + '\n'
+        if self.email is not None:
+            s += self.email
+        print(s)
+
+
+PhB1 = Phone_book('Lady Gaga', office_phone='3054778230',
+                  email='gagahulala@badromance.com')
+PhB2 = Phone_book('David Copperfield', office_phone='infinity')
+PhB2.add_email('loststatue@newliberty.com')
+phone_notebook = [PhB1, PhB2]
+
+for person in phone_notebook:
+    print(person.dump())
