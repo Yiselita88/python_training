e8bf0dd18ddb8b8365c3cb985b9218dac9404c1e
Yisel M Noa <yisel05371@gmail.com>
2024-10-07 18:09:53 -0400

    fancy indexing and applications

--- python_Data_Science/broadcasting.py
+++ python_Data_Science/broadcasting.py
@@ -31,3 +31,54 @@ print(np.less(b, 4))
 # Boolean arrays and masks (examples)
 summer = (np.arange(365) - 172 < 90) & (np.arange(365) - 172 > 0)
 print(summer)
+
+# fancy indexing: when passing an array of indices to access multiple array elements at once
+import numpy as np
+matrx = np.arange(12).reshape(3, 4)
+print(matrx)
+rw = np.array([0, 1, 2])
+clmn = np.array([2, 1, 3])
+desire_vl = matrx[rw, clmn]
+print(desire_vl)
+
+# combine indexing
+print(matrx[2, [3, 0, 2]])
+# slicing & combine indexing
+print(matrx[1:, [3, 2, 1]])
+
+# example of fancy indexing application
+mean = [0, 0]
+cov = [[1, 2],
+       [2, 5]]
+data_1 = np.random.multivariate_normal(mean, cov, 100)
+print(data_1.shape)
+
+indices = np.random.choice(data_1.shape[0], 20, replace=False)
+print(indices)
+selection = data_1[indices]
+print(selection.shape)
+
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn
+seaborn.set()
+
+# plt.scatter(data_1[:, 0], data_1[:, 1])
+
+#plt.scatter(data_1[:, 0], data_1[:, 1], alpha=0.3)
+#plt.scatter(selection[:, 0], selection[:, 1], color='red', s=200)
+# plt.show()
+
+# example for bining data
+np.random.seed(42)
+x = np.random.randn(100)
+
+bins = np.linspace(-5, 5, 20)
+counts = np.zeros_like(bins)
+
+i = np.searchsorted(bins, x)
+np.add.at(counts, i, 1)
+#plt.plot(bins, counts, linestyle='solid')
+
+plt.hist(x, bins, histtype='step')
+plt.show()
